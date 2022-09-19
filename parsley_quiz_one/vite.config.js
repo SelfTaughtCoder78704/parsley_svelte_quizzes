8215b8cddafef96fca63cefd154a69348483e4f0
SelfTaughtCoder78704 <bobbynicholson78704@gmail.com>
2022-09-19 14:04:26 -0500

    updated config

--- parsley_quiz_one/vite.config.js
+++ parsley_quiz_one/vite.config.js
@@ -2,6 +2,32 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()]
+// })
+
+
+// use svelte plugin and export the js file with the name of index.js
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'index'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['svelte'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          svelte: 'Svelte'
+        }
+      }
+    }
+  }
 })
+
+
